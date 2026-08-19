110f6429ae14dffa812403308cfc5df9ec868595
Vladimir <volodya.klimenko.00.00@mail.ru>
2026-08-19 19:51:24 +0300

    Присвоение переменным разных значений

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,44 @@
 public class Main {
-    static void main(){
-        var dog= 8.0;
-        var cat= 3.6;
-        var paper= 763789;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);}
+    static void main() {
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("значение#1"+" "+dog);
+        System.out.println("значение#1"+" "+cat);
+        System.out.println("значение#1"+" "+paper);
+
+
+        var dog2 = 8.0 + 4;
+        var cat2 = 3.6 + 4;
+        var paper2 = 763789 + 4;
+        System.out.println("значение#2"+" "+dog2);
+        System.out.println("значение#2"+" "+cat2);
+        System.out.println("значение#2"+" "+paper2);
+
+
+        var dog3 = 8.0 - 3.5;
+        var cat3 = 3.6 - 1.6;
+        var paper3 = 763789 - 7639;
+        System.out.println("значение#3"+" "+dog3);
+        System.out.println("значение#3"+" "+cat3);
+        System.out.println("значение#3"+" "+paper3);
+
+
+        var friend = 19;
+        var friendby2 = friend + 2;
+        var friendby7 = friendby2 / 7;
+        System.out.println("значение#4"+" "+friend);
+        System.out.println("значение#4"+" "+friendby2);
+        System.out.println("значение#4"+" "+friendby7);
+
+
+        var frog=3.5;
+        var frogby10=frog*10;
+        var frogby35=frogby10/3.5;
+        var frogby4=frogby35+4;
+        System.out.println("значение#5"+" "+frog);
+        System.out.println("значение#5"+" "+frogby10);
+        System.out.println("значение#5"+" "+frogby35);
+        System.out.println("значение#5"+" "+frogby4);
+    }
 }
