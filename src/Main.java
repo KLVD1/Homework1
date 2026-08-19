e9c79f85ef3900bede2638174dc3f316c2e010fa
Vladimir <volodya.klimenko.00.00@mail.ru>
2026-08-19 18:52:35 +0300

    Присвоение переменным разных значений

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     static void main(){
-        var dog= 8.0+4;
-        var cat= 3.6+4;
-        var paper= 763789+4;
+        var dog= 8.0*3.5;
+        var cat= 3.6*3.5;
+        var paper= 763789*3.5;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);}
